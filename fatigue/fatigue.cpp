6847c1a5174b106de255f2dc69bf93c5e0703f55
Hutch <clutterskull@gmail.com>
2024-10-21 14:08:50 -0700

    Maps, refactor lambdas

--- fatigue/fatigue.cpp
+++ fatigue/fatigue.cpp
@@ -1,10 +1,7 @@
 #include <cstring>
 #include <dirent.h>
 #include <filesystem>
-#include <format>
 #include <fstream>
-#include <map>
-#include <string>
 #include "fatigue.hpp"
 #include "log.hpp"
 
@@ -46,7 +43,6 @@ namespace fatigue {
             }
         } catch (const std::exception& e) {
             logError(std::format("Error reading {}: {}", path.c_str(), e.what()));
-            return status;
         }
 
         return status;
@@ -85,8 +81,9 @@ namespace fatigue {
         }
     }
 
-    pid_t getProcessID(const std::string& processName,
-                       bool (*comparator)(const std::string&, const pid_t))
+    // Process ID
+
+    pid_t getProcessID(const std::string& processName, std::function<bool(pid_t)> filter)
     {
         if (processName.empty()) return 0;
 
@@ -97,7 +94,7 @@ namespace fatigue {
             if (entry.is_directory()) {
                 pid_t entryPid = atoi(entry.path().filename().c_str());
                 if (entryPid > 0) {
-                    if (comparator(processName, entryPid)) {
+                    if (filter(entryPid)) {
                         pid = entryPid;
                         break;
                     }
@@ -110,29 +107,110 @@ namespace fatigue {
     }
 
     pid_t getProcessId(const std::string& cmdline) {
-        return getProcessID(cmdline, [](const std::string& processName, const int checkPid) {
-            return getCmdline(checkPid) == processName;
+        return getProcessID(cmdline, [&](const int checkPid) {
+            return getCmdline(checkPid) == cmdline;
         });
     }
 
     pid_t getProcessIdByStatusName(const std::string& processName)
     {
-        return getProcessID(processName, [](const std::string& processName, const int checkPid) {
+        return getProcessID(processName, [&](const int checkPid) {
             return getStatusName(checkPid) == processName;
         });
     }
 
     pid_t getProcessIdByCmdlineEndsWith(const std::string& processName)
     {
-        return getProcessID(processName, [](const std::string& processName, const int checkPid) {
+        return getProcessID(processName, [&](const int checkPid) {
             return getCmdline(checkPid).ends_with(processName);
         });
     }
 
     pid_t getProcessIdByCmdlineContains(const std::string& processName)
     {
-        return getProcessID(processName, [](const std::string& processName, const int checkPid) {
+        return getProcessID(processName, [&](const int checkPid) {
             return getCmdline(checkPid).contains(processName);
         });
     }
+
+    // Maps
+
+    std::vector<Map> getMaps(pid_t pid, std::function<bool(Map&)> filter)
+    {
+        std::vector<Map> maps{};
+        if (pid <= 0) return maps;
+
+        std::filesystem::path path = std::format("/proc/{}/maps", pid);
+        std::ifstream file(path);
+        if (!file.is_open()) {
+            logError(std::format("Error opening {}", path.c_str()));
+            return maps;
+        }
+
+        try {
+            std::string line;
+            while (std::getline(file, line)) {
+                Map map{};
+                map.pid = pid;
+
+                // Throwaway variable for the dash
+                char dash;
+                // (format) startAddress-endAddress perms offset dev inode pathname
+                std::istringstream iss{line};
+                iss >> map.startAddress >> dash >> map.endAddress
+                    >> map.perms >> map.offset >> map.dev >> map.inode >> map.pathname;
+
+                if (!filter || filter(map)) {
+                    maps.push_back(map);
+                }
+            }
+        } catch (const std::exception& e) {
+            logError(std::format("Error reading {}: {}", path.c_str(), e.what()));
+        }
+
+        return maps;
+    }
+
+    std::vector<Map> getValidMaps(pid_t pid)
+    {
+        return getMaps(pid, [](Map& map) {
+            return map.isValid() && map.hasName();
+        });
+    }
+
+    std::vector<Map> getMaps(pid_t pid, const std::string& name)
+    {
+        return getMaps(pid, [&name](Map& map) {
+            return map.isValid() && map.pathname.contains(name);
+        });
+    }
+
+    std::vector<Map> getMapsEndsWith(pid_t pid, const std::string& name)
+    {
+        return getMaps(pid, [&name](Map& map) {
+            return map.isValid() && map.pathname.ends_with(name);
+        });
+    }
+
+    Map findMap(pid_t pid, const std::string& name)
+    {
+        std::vector<Map> maps = getMaps(pid, name);
+        if (maps.empty()) return Map{};
+        return maps.front();
+    }
+
+    Map findMapEndsWith(pid_t pid, const std::string& name)
+    {
+        std::vector<Map> maps = getMapsEndsWith(pid, name);
+        if (maps.empty()) return Map{};
+        return maps.front();
+    }
+
+    // Memory
+
+    std::vector<int16_t> read(pid_t pid, uintptr_t address, size_t size)
+    {
+        return {};
+    }
+
 } // namespace fatigue
--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ void testLog() {
 int main(int argc, char* args[])
 {
     // log::setLogFormat(log::LogFormat::Default);
-    log::setLogFormat(log::LogFormat::Tiny);
+    log::setLogFormat(log::LogFormat::Compact);
+    // log::setLogFormat(log::LogFormat::Tiny);
     log::setLogLevel(log::LogLevel::Debug);
 
     // testLog();
@@ -25,27 +26,27 @@ int main(int argc, char* args[])
     logInfo("Step 1: Find Sekiro.exe");
 
     std::string processName = "sekiro.exe";
-
-    // Attach to process
-    // Fatigue *process = Fatigue::attach(processName, &getProcessIDByStatusName);
-
-    logInfo(std::format("Process Name: {}", processName.c_str()));
-
-    pid_t pid = getProcessIdByCmdlineEndsWith("zen-bin");
-    logInfo(std::format("Process ID: {}", pid));
-
+    pid_t pid = getProcessIdByStatusName(processName);
     std::string statusName = getStatusName(pid);
 
-    logInfo(std::format("Status Name: {}", statusName.c_str()));
+    logInfo(std::format("Process Name: {}", processName.c_str()));
+    logInfo(std::format("Process ID:   {}", pid));
+    logInfo(std::format("Status Name:  {}", statusName.c_str()));
 
-    // KITTY_LOGI("Process ID:   %d", process->processID());
-    // KITTY_LOGI("Process Cmd:  %s", process->processName().c_str());
     // KITTY_LOGI("Located PE:   %s", process->peScanner.isValid() ? "Yes" : "No");
 
     // if (!process->peScanner.isValid()) {
     //     KITTY_LOGE("Failed to locate PE headers");
     //     return 1;
     // }
+
+    logInfo("Step 2: Get process maps");
+
+    // std::vector<Map> maps = getValidMaps(pid);
+    // std::vector<Map> maps = getMaps(pid, processName);
+    Map map = findMap(pid, processName);
+
+    logInfo(map.toString());
 
     // KITTY_LOGI("Found map: %s", process->peScanner.getProcMap()->toString().c_str());
 
